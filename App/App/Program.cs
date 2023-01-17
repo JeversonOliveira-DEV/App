5d82a200e2387988b9ceb3a497c6c135a1e89dfd
DevJevy <jeverson.mano@hotmail.com>
2023-01-17 17:27:11 -0300

    Declaração de Variaveis

---
+++ App/App/Program.cs
@@ -0,0 +1,27 @@
+﻿/*string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Beth";
+Console.WriteLine(firstName);
+firstName = "Conrad";
+Console.WriteLine(firstName);
+firstName = "Grant";
+Console.WriteLine(firstName);
+Console.ReadLine();*/
+
+string firstName = "Hello, Bob!";
+string f = "3";
+string v = "34,4";
+Console.Write("  ");
+Console.Write("You have");
+Console.Write(" ");
+Console.Write(f);
+Console.Write(" ");
+Console.Write("messages in your inbox.");
+Console.Write(" ");
+Console.Write("The temperature is ");
+Console.Write(" ");
+Console.Write(v);
+Console.Write(" ");
+Console.Write("celsius.");
+Console.ReadLine();
