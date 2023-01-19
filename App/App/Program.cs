67fc15f44d7c0894c54d2e7b770fbc11ad901ab8
DevJevy <jeverson.mano@hotmail.com>
2023-01-19 12:00:08 -0300

    Executar operações básicas em números em C#

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -62,7 +62,7 @@ string projectName = "First-Project";
 Console.WriteLine($@"C:\Output\{projectName}\Date");
 Console.ReadLine();*/
 
-string projectName = "ACME";
+/*string projectName = "ACME";
 
 string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 
@@ -70,8 +70,80 @@ Console.WriteLine($@"View English output:
      C:\Exercise\{projectName}\data.text");
 
 Console.WriteLine($@"{russianMessage}
-     C:\Exercise\{projectName}\data.text");
+     C:\Exercise\{projectName}\data.text");*/
 
+/*string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");*/
 
+/*int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
 
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);*/
+
+/*decimal decimalQuotient = 7 / 5.0m;
+//decimal decimalQuotient = 7.0m / 5.0m;
+Console.WriteLine("Decimal quotient: " + decimalQuotient);*/
+
+/*int first = 7;
+int second = 5;
+decimal quotient = (decimal)first / (decimal)second;
+Console.WriteLine(quotient);*/
+
+/*Console.WriteLine("Modulus of 200 / 5 : " + (200 % 5));
+Console.WriteLine("Modulus of 7 / 5 : " + (7 % 5));*/
+
+/*int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);*/
+
+/*int value = 1;
+
+value = value + 1;
+Console.WriteLine("First increment: " + value);
+
+value += 1;
+Console.WriteLine("Second increment: " + value);
+
+value++;
+Console.WriteLine("Third increment: " + value);
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value);
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value);
+
+value--;
+Console.WriteLine("Third decrement: " + value);*/
+
+/*int value = 1;
+value++;
+Console.WriteLine("First: " + value);
+Console.WriteLine("Second: " + value++);
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));*/
+
+//Converter Fahrenheit para Celsius. 
+/*int fahrenheit = 94;
+decimal Celsius = ((decimal)fahrenheit - 32) * 5 / 9;
+Console.WriteLine($"The temperature is {Celsius} Celsius.");*/
+
+// Outra solução.
+/*int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+Console.WriteLine("The temperature is " + celsius + " Celsius.");*/
+
+//Console.WriteLine(5m / 10m);//Solução tem como resultado zero porque ao ta declarado como decimal.
+//Console.WriteLine(5m / 10m);//Manueira correta de usar asolução.
+
+Console.WriteLine(1 && 5);
 Console.ReadLine();
